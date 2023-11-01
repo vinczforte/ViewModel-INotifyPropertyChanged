3a1d3520554602b977375102008a70a000ab78d1
Vitaly Sidorov <83981529+vinczforte@users.noreply.github.com>
2023-11-01 10:11:39 +0300

    Add files via upload

---
+++ ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace TISBIApp.ViewModelsFile.BaseClasses
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = "")
+        { 
+           
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName)); 
+        }
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+  
+
+    
+}
